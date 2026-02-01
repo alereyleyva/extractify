d105f6bcad11fb6b0839b1f60eef303bfa9692c4
Alejandro Rey <alereyleyva@gmail.com>
2026-02-01 09:05:03 +0100

    chore: initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@extractify/db";
+import * as schema from "@extractify/db/schema/auth";
+import { env } from "@extractify/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [tanstackStartCookies()],
+});
